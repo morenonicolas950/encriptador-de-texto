4d4297f866db8db1d1920893713315557631066e
Moreno Nicolas <142637312+morenonicolas950@users.noreply.github.com>
2024-08-19 14:10:39 -0300

    version final

--- app.js
+++ app.js
@@ -9,46 +9,51 @@ function encriptarTexto(texto) {
     if (!prohibido.test(texto)) {
         return alert("El texto no puede contener caracteres, mayúsculas o acentos");
     }
-     
+    else{
     return texto.replace(/e/g, "enter")
     .replace(/i/g, "imes")
     .replace(/a/g,"ai")
     .replace(/o/g, "ober")
     .replace(/u/g, "ufat");
+    }
 } 
 
 function encriptar(){
 
     const textoplano = document.getElementById('textAreaCentral').value;
 
-    if(textoplano==""){
+    if(textoplano.length === 0){
 
         alert("Debe ingresar un mensaje para encriptar");
     }
-    else{
-
-    let textoCifrado = encriptarTexto(textoplano);
-
-    Limpiar("textAreaCentral");
-
-    ocultarElementos();
    
-    document.getElementById("textAreaLateral").innerHTML = textoCifrado;
+    else{ 
 
-    }  
-}
+        let textoCifrado = encriptarTexto(textoplano);
 
+        if(textoCifrado.length > 0){
 
+        Limpiar("textAreaCentral");
+
+        ocultarElementos();
+
+        document.getElementById("textAreaLateral").style.height = "400px";
+    
+        document.getElementById("textAreaLateral").value = textoCifrado;
+
+         }
+      }  
+
+    }
 
 
 function desencriptarTexto(textoCifrado) {
 
     const prohibido = /^[a-z\s]+$/;
 
-    if (!prohibido.test(textoCifrado)) {
+    if (!prohibido.test(textoCifrado) ) {
         return alert("El texto no puede contener caracteres, mayúsculas o acentos");
     }
-
     return textoCifrado
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
@@ -63,12 +68,15 @@ function desencriptar() {
 
     if (textoEncriptado==""){
 
-        alert("Debe ingresar un mensaje");
+        alert("Debe ingresar un mensaje para desencriptar");
+
     }
 
     else {
- 
+        
     const textoDesencriptado = desencriptarTexto(textoEncriptado);
+
+    if(textoDesencriptado.length > 0){
 
     Limpiar("textAreaCentral");
 
@@ -77,8 +85,10 @@ function desencriptar() {
     Limpiar("textAreaLateral");
 
     mostrarElementos();
-
+        
+         }
     }
+
 }
 
 
@@ -87,7 +97,8 @@ function desencriptar() {
 function Limpiar(id){
 
     const textarea = document.getElementById(id);
-    textarea.innerHTML = "";
+
+    textarea.value = "";
 };
 
 function ocultarElementos(){
@@ -108,3 +119,24 @@ document.getElementById("parrafosLaterales").style.display= "block";
 
 document.getElementById("btnCopiar").style.display = "none" ;
 }
+
+async function copiarContenido() {
+
+    let texto = document.getElementById("textAreaLateral").value;
+
+    try {
+
+      await navigator.clipboard.writeText(texto);
+
+      alert('Contenido copiado al portapapeles');
+
+      /* Resuelto - texto copiado al portapapeles con éxito */
+
+    } catch (error) {
+
+      alert('Error al copiar: ', error);
+
+      /* Rechazado - fallo al copiar el texto al portapapeles */
+
+    }
+  }
